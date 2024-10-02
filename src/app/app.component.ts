d1a986cdfa02d3ad585c8cb28a6ad2797df9b580
XaerickSReborn <u202213358@upc.edu.pe>
2024-10-01 19:42:47 -0500

    feat(laureates): implemented laureates item and list visualization.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FooterContentComponent} from "./public/components/footer-content/footer-content.component";
+import {TranslateService} from "@ngx-translate/core";
+import {LanguageSwitcherComponent} from "./public/components/language-switcher/language-switcher.component";
+import {MatToolbar} from "@angular/material/toolbar";
+import {LaureteListComponent} from "./laureates/components/laurete-list/laurete-list.component";
+import {MatCardHeader, MatCardSubtitle, MatCardTitle} from "@angular/material/card";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterContentComponent, LanguageSwitcherComponent, MatToolbar, LaureteListComponent, MatCardSubtitle, MatCardTitle, MatCardHeader],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'repasopc1';
+
+  constructor(translate: TranslateService) {
+    translate.setDefaultLang('es');
+    translate.use('en');
+  }
 }
